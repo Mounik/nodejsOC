e1a45a64038749c223bc1191f8f6fb6e50c75ccc
Mounik <mounicou@gmail.com>
2019-03-04 18:32:24 +0100

    Début avec Node.js
    Installation d'un serveur et récupération des paramètres dans l'URL.
    http://localhost:8080?prenom=Robert&nom=Dupont

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # nodejsOC
+Apprentissage de Node.js à l'aide du cours OpenClassrooms
+https://openclassrooms.com/fr/courses/1056721-des-applications-ultra-rapides-avec-node-js
--- serveur.js
+++ serveur.js
@@ -1,11 +1,16 @@
 var http = require('http');
-var url = required("url");
+var url = require('url');
+var querystring = require('querystring');
 
 var server = http.createServer(function(req, res) {
-  var page = url.parse(req.url).pathname;
-  console.log(page);
+    var params = querystring.parse(url.parse(req.url).query);
     res.writeHead(200, {"Content-Type": "text/plain"});
-    res.write('Bien le bonjour');
+    if ('prenom' in params && 'nom' in params) {
+        res.write('Vous vous appelez ' + params['prenom'] + ' ' + params['nom']);
+    }
+    else {
+        res.write('Vous devez bien avoir un prénom et un nom, non ?');
+    }
     res.end();
 });
 server.listen(8080);
